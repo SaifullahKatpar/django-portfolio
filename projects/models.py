3d597aa7d300b8c3993c4d7c8e43ff759b20ea86
SaifullahKatpar <saifullah.cs14@iba-suk.edu.pk>
2022-07-21 15:42:19 +0500

    updated home page and detail page layout

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Project(models.Model):
 	title = models.CharField(max_length=40,default='Project')
 	image = models.ImageField(upload_to='images/')
-	summary = models.CharField(max_length=200)
-	link = models.CharField(max_length=100,default='#')
+	summary = models.CharField(max_length=5000)
+	link = models.CharField(max_length=2000,default='#')
 
 	def __str__(self):
 		return self.title
